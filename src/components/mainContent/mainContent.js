9f7452c647169bdf3863f6dae2f2b889e573e82b
Mona Lisa <official.suprabh@gmail.com>
2018-10-20 18:53:16 +0530

    msg

--- src/components/mainContent/mainContent.js
+++ src/components/mainContent/mainContent.js
@@ -5,8 +5,8 @@ const mainContent = ()=>{
     const pathNames = window.location.pathname.split('/');
     let ques = null;
     if(pathNames.length>2)  {
-        const path1 = pathNames[1];
-        const path2 = pathNames[2];
+        const path1 = pathNames[2];
+        const path2 = pathNames[3];
         const questionsArray = questions[path1][path2].questions;
         ques = questionsArray.map((questionObj, index)=>(
             <h3 key={index}>{questionObj.ques}</h3>
