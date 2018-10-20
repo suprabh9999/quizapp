9b7d509f4a407d8cabd371b9e12c4cd1486fa743
Mona Lisa <official.suprabh@gmail.com>
2018-10-20 18:41:42 +0530

    Updates

---
+++ precache-manifest.7e7e0e77d3d16dbf66be60e0d994dcfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "777894a7739f50d88b81",
+    "url": "/quizapp/static/js/runtime~main.777894a7.js"
+  },
+  {
+    "revision": "51f8a3480aa4b5b8ca2e",
+    "url": "/quizapp/static/js/main.51f8a348.chunk.js"
+  },
+  {
+    "revision": "691c5e3d50b0f86bc3a0",
+    "url": "/quizapp/static/js/1.691c5e3d.chunk.js"
+  },
+  {
+    "revision": "51f8a3480aa4b5b8ca2e",
+    "url": "/quizapp/static/css/main.8df2bfb1.chunk.css"
+  },
+  {
+    "revision": "ae86537c566f1355898a0f02bafd8dfc",
+    "url": "/quizapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2fd096edaf18ffb34100367493a7e77f.js"
+  "/quizapp/precache-manifest.7e7e0e77d3d16dbf66be60e0d994dcfb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/quizapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
